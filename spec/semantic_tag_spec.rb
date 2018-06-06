b1e89c3ec37ed36dc25932a07d6e518915acf026
maxwell <github email address>
2018-06-06 15:31:26 -0400

    minor adjustments to tests

--- spec/semantic_tag_spec.rb
+++ spec/semantic_tag_spec.rb
@@ -1,20 +1,35 @@
 require 'pry'
 
 RSpec.describe 'index.html' do
+
+  it 'has a valid document structure' do
+
+  expect(html_file_contents).to include('<html')
+  expect(html_file_contents).to include('</html>')
+
+  validator = W3CValidators::NuValidator.new
+  html = File.read('./index.html')
+  results = validator.validate_text(html)
+
+  error_messages = "Expected a valid document but got:\n#{results.errors.collect{|e| e.to_s}.join("\n")}"
+
+  expect(results.errors).to be_empty, error_messages
+end
+
   context 'within <body>' do
-    it 'contains an <header> tag' do
+    it 'contains a <header> tag' do
       header = parsed_html.search('header')[0]
       expect(header).to_not be_nil, "No <header> tag was found"
       expect(html_file_contents).to include('</header>'), "No closing </header> tag was found"
     end
 
-    it 'contains an <nav> tag' do
+    it 'contains a <nav> tag' do
       nav = parsed_html.search('nav')[0]
       expect(nav).to_not be_nil, "No <nav> tag was found"
       expect(html_file_contents).to include('</nav>'), "No closing </nav> tag was found"
     end
 
-    it 'contains an <main> tag' do
+    it 'contains a <main> tag' do
       main = parsed_html.search('main')[0]
       expect(main).to_not be_nil, "No <main> tag was found"
       expect(html_file_contents).to include('</main>'), "No closing </main> tag was found"
@@ -24,7 +39,6 @@ RSpec.describe 'index.html' do
       it 'contains three separate <section> elements' do
         section = parsed_html.search('section')
         expect(section).to_not be_nil, "No <section> tags were found"
-        expect(html_file_contents).to include('</section>'), "No closing </section> tag was found"
         expect(section.length).to be >= 3, "Page should contain 3 sets of section tags"
       end
 
@@ -50,6 +64,7 @@ RSpec.describe 'index.html' do
 
       it 'contains a <figure> tag within the second <section>' do
         section = parsed_html.search('section')[1]
+        expect(section).to_not be_nil, "No <section> tags were found"
         figure = section.children.select {|ch| ch.name == 'figure'}[0]
 
         expect(figure).to_not be_nil, "No <figure> tag found within the second section"
@@ -61,7 +76,7 @@ RSpec.describe 'index.html' do
       end
     end
 
-    it 'contains an <footer> tag' do
+    it 'contains a <footer> tag' do
       footer = parsed_html.search('footer')[0]
       expect(footer).to_not be_nil, "No <footer> tag was found"
       expect(html_file_contents).to include('</footer>'), "No closing </footer> tag was found"
@@ -74,7 +89,7 @@ RSpec.describe 'index.html' do
         expect(html_file_contents).to include('</address>'), "No closing </address> tag was found"
       end
 
-      it 'contains an <details> tag' do
+      it 'contains a <details> tag' do
         details = parsed_html.search('details')[0]
         expect(details).to_not be_nil, "No <details> tag was found"
         expect(html_file_contents).to include('</details>'), "No closing </details> tag was found"
